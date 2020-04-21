ad18ef36b8cf11031fd169ddca98dd2468603bd7
Alexander Bleissem <alexander@bleissem.com>
2020-04-21 21:37:04 +0200

    initial commit

---
+++ TinyScaffold/TinyScaffold/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TinyScaffold
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from TinyScaffold");
+        }
+    }
+}
